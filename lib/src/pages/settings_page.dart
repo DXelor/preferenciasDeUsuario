46c51be6c7a991ffefe542c423d5f06660e54cb7
DXelor <dzaliaspro@gmail.com>
2021-01-23 18:24:48 -0500

    Crear un Widget independiente para el drawer

--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:preferenciausuarioapp/src/widgets/menu_widget.dart';
 
 class SettingsPage extends StatelessWidget {
   static final String routeName = 'settings';
@@ -10,6 +11,7 @@ class SettingsPage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Ajustes'),
         ),
+        drawer: MenuWidget(),
         body: Center(
           child: Text('Ajustes Page'),
         ),
